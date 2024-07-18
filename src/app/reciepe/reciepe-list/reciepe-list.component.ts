deca1e87878a5b7fe3fbdd8ccf9d32b7e885dea2
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-07-18 10:44:49 +0400

    c

--- src/app/reciepe/reciepe-list/reciepe-list.component.ts
+++ src/app/reciepe/reciepe-list/reciepe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 import { ReciepeItemComponent } from './reciepe-item/reciepe-item.component';
 
 @Component({
@@ -8,4 +9,12 @@ import { ReciepeItemComponent } from './reciepe-item/reciepe-item.component';
   templateUrl: './reciepe-list.component.html',
   styleUrl: './reciepe-list.component.scss',
 })
-export class ReciepeListComponent {}
+export class ReciepeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'this is simple res',
+      'https://www.southernliving.com/thmb/jM1YjcVqzkt-Ej6pMp7qK--c_9Q=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg'
+    ),
+  ];
+}
