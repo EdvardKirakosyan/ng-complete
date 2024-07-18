d4568761cd1982e111a26a08950919e19457b641
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-07-18 11:33:40 +0400

    c

--- src/app/reciepe/reciepe-list/reciepe-list.component.ts
+++ src/app/reciepe/reciepe-list/reciepe-list.component.ts
@@ -16,5 +16,15 @@ export class ReciepeListComponent {
       'this is simple res',
       'https://www.southernliving.com/thmb/jM1YjcVqzkt-Ej6pMp7qK--c_9Q=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg'
     ),
+    new Recipe(
+      'A test Recipe',
+      'this is simple res',
+      'https://www.southernliving.com/thmb/jM1YjcVqzkt-Ej6pMp7qK--c_9Q=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg'
+    ),
+    new Recipe(
+      'A test Recipe',
+      'this is simple res',
+      'https://www.southernliving.com/thmb/jM1YjcVqzkt-Ej6pMp7qK--c_9Q=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg'
+    ),
   ];
 }
